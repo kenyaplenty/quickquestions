acfb0a83f5358eb8688dd83e18fd39b16bdf209d
Karime <karime.robles@hotmail.com>
2020-10-07 21:34:07 -0400

    Initial Commit

---
+++ quick-questions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quick-questions
+//
+//  Created by karime on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
