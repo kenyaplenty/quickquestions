587749b5f8a625ccea5175d18afe7cf30ab3eec5
Francesco Prospato <fprospato@gmail.com>
2020-10-13 16:33:35 -0700

    Add 3 options and have logic for selecting them

--- quick-questions/ViewController.swift
+++ quick-questions/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    
+    @IBOutlet weak var option1Btn: UIButton!
+    @IBOutlet weak var option2Btn: UIButton!
+    @IBOutlet weak var option3Btn: UIButton!
+    
+    //MARK: - Variables
+    var selectedOption = 0
+    
+    
+    //MARK: - View functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    //MARK: - Button actions
+    @IBAction func option1Tap(_ sender: Any) {
+        selectOption1()
+    }
+    
+    func selectOption1() {
+        selectedOption = 1
+    }
+    
+    @IBAction func option2Tap(_ sender: Any) {
+        selectOption2()
+    }
+    
+    func selectOption2() {
+        selectedOption = 2
+    }
+    
+    @IBAction func option3Tap(_ sender: Any) {
+        selectOption3()
+    }
+    
+    func selectOption3() {
+        selectedOption = 3
+    }
 }
 
